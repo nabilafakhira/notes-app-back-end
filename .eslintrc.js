bde40e63080f436d81925c7d40e93b46fa784521
Nabila Fakhiratunisa <nabilafakhiratunisa@gmail.com>
2022-07-26 09:16:09 +0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+    },
+    "extends": [
+        "eslint:recommended",
+        // "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
